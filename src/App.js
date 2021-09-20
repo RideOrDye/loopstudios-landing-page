5a67a73f5a6f4575fe6b4dcd31e8c7637a6e8afe
Vianney <vianneysalmeron@gmail.com>
2021-09-19 17:38:09 -0700

    header and showcase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from './components/Header'
+import Showcase from './components/Showcase';
 
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div>Loop Studios</div>
+    <>
+    <Header/>
+    <Showcase />
+    </>
   );
 }
 
